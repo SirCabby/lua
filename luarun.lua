7e0642d96e5ffdb8a2ab1553a00e91f272e808d9
SirCabby <77202713+SirCabby@users.noreply.github.com>
2022-09-24 14:07:17 -0400

    add regions and do-block for start

--- luarun.lua
+++ luarun.lua
@@ -1,13 +1,14 @@
--- includes
+--region includes===========================================================
 local mq = require("mq")
 require "filesystem.filesystem"
+--endregion includes===========================================================
 
--- fields
+--region fields=================================================================
 local args = {...}
 local LUA_DIR = mq.TLO.Lua.Dir()
+--endregion fields=================================================================
 
--- local functions
-
+--region local functions=========================================================
 --- Adds ".lua" to end of supplied fileName
 local function AddLuaExtensionIfMissing(fileName)
     if not string.find(string.lower(fileName), ".lua") then
@@ -15,25 +16,29 @@ local function AddLuaExtensionIfMissing(fileName)
     end
     return fileName
 end
+--endregion local functions=========================================================
 
--- start
-if args[1] == nil then
-    print("Must supply a lua file name to run. Usage:")
-    print("-- /lua run luarun somefile.lua")
-    print("Alternatively, setup an alias via:")
-    print("-- /alias /luar /lua run luarun somefile.lua")
-    print("Alias usage:")
-    print("-- /luar somefile.lua")
-    mq.exit()
+--region start===================================================================
+do
+    if args[1] == nil then
+        print("Must supply a lua file name to run. Usage:")
+        print("-- /lua run luarun somefile.lua")
+        print("Alternatively, setup an alias via:")
+        print("-- /alias /luar /lua run luarun somefile.lua")
+        print("Alias usage:")
+        print("-- /luar somefile.lua")
+        mq.exit()
+    end
+
+    local luaFileName = AddLuaExtensionIfMissing(args[1])
+    local luaFile = FindFile(LUA_DIR, luaFileName, true)
+    if luaFile == nil then
+        print("Unable to find lua file to run: "..luaFileName)
+        mq.exit()
+    else
+        print("Found file: "..luaFile)
+    end
+
+    mq.cmdf("/lua run %s", luaFile)
 end
-
-local luaFileName = AddLuaExtensionIfMissing(args[1])
-local luaFile = FindFile(LUA_DIR, luaFileName, true)
-if luaFile == nil then
-    print("Unable to find lua file to run: "..luaFileName)
-    mq.exit()
-else
-    print("Found file: "..luaFile)
-end
-
-mq.cmdf("/lua run %s", luaFile)
+--endregion start===================================================================
