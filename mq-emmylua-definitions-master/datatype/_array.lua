2b15af96e57e7af6c0da87b691c11be84bee8a1a
SirCabby <77202713+SirCabby@users.noreply.github.com>
2022-09-26 15:31:20 -0400

    remove warnings

--- mq-emmylua-definitions-master/datatype/_array.lua
+++ mq-emmylua-definitions-master/datatype/_array.lua
@@ -1,5 +1,5 @@
 --- @class array
---- @deprecated
+--- 
 --- @field public Dimensions number Number of dimensions in the array
 --- @field public Size number Total number of elements in the array
 --- @field public ToString string None
