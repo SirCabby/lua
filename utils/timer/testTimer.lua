9c463d59686ec1d8fc6604a790e901b46096fc77
SirCabby <77202713+SirCabby@users.noreply.github.com>
2022-09-26 23:36:24 -0400

    auto start timer and fixed time ending on 0

--- utils/timer/testTimer.lua
+++ utils/timer/testTimer.lua
@@ -1,19 +1,11 @@
+---@type Mq
 local mq = require('mq')
 local Timer = require "utils.timer.timer"
 
+mq.cmd("/mqclear")
+
 local my_timer = Timer:new(10)
--- by default, timer begins expired because initial start time is 0, so this loop ends immediately
-while true do
-    if my_timer:timer_expired() then
-        print('timer expired')
-        break
-    else
-        print('not yet')
-    end
-    mq.delay(1000)
-end
--- reset sets start time to current time, so it will take full expiration time after that
-my_timer:reset()
+
 while true do
     if my_timer:timer_expired() then
         print('timer expired')
--- utils/timer/timer.lua
+++ utils/timer/timer.lua
@@ -6,7 +6,7 @@ local Timer = {
     start_time = 0
 }
 
----Initialize a new timer istance.
+---Initialize a new timer instance
 ---@param expiration number @The number of seconds after the start time which the timer will be expired
 ---@return Timer @The timer instance
 function Timer:new(expiration)
@@ -15,35 +15,36 @@ function Timer:new(expiration)
     self.__index = self
     t.start_time = 0
     t.expiration = expiration
+    t:reset()
     return t
 end
 
----Return the current time in seconds.
----@return number @Returns a number representing the current time in seconds.
+---Return the current time in seconds
+---@return number @Returns a number representing the current time in seconds
 function Timer.current_time()
     return os.time()
 end
 
----Reset the start time value to the current time.
+---Reset the start time value to the current time
 ---@param expiration number @The number of seconds after the start time which the timer will be expired
 function Timer:set(expiration)
     Timer:reset()
     self.expiration = expiration
 end
 
----Reset the start time value to the current time.
+---Reset the start time value to the current time
 function Timer:reset()
     self.start_time = Timer.current_time()
 end
 
----Check whether the specified timer has passed its expiration.
----@return boolean @Returns true if the timer has expired, otherwise false.
+---Check whether the specified timer has passed its expiration
+---@return boolean @Returns true if the timer has expired, otherwise false
 function Timer:timer_expired()
-    return os.difftime(Timer.current_time(), self.start_time) > self.expiration
+    return os.difftime(Timer.current_time(), self.start_time) >= self.expiration
 end
 
----Get the time remaining before the timer expires.
----@return number @Returns the number of seconds remaining until the timer expires.
+---Get the time remaining before the timer expires
+---@return number @Returns the number of seconds remaining until the timer expires
 function Timer:time_remaining()
     return self.expiration - os.difftime(Timer.current_time(), self.start_time)
 end
