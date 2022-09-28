0392578b80414e6f68a0bc26578929695c1c97bf
SirCabby <77202713+SirCabby@users.noreply.github.com>
2022-09-27 20:13:44 -0400

    add luautils

---
+++ utils/luautil/test/all.lua
@@ -0,0 +1,14 @@
+---@diagnostic disable: lowercase-global
+lu = require('test.luaunit')
+
+require 'test.misc'
+require 'test.io'
+-- lfs requires mocking out lfs.
+require 'test.string'
+-- math
+-- table
+require 'test.logging'
+require 'test.flags'
+
+runner = lu.LuaUnit.new()
+os.exit( runner:runSuite() )
