2ce222dbf620da1ee75bb7de7075fca05309a598
SirCabby <77202713+SirCabby@users.noreply.github.com>
2022-10-02 18:01:39 -0400

    add file read/write and path join

--- utils/FileSystem.lua
+++ utils/FileSystem.lua
@@ -1,10 +1,9 @@
 local PackageMan = require("mq/PackageMan")
 local lfs = PackageMan.Require("luafilesystem", "lfs")
+local StringUtils = require("utils.StringUtils.StringUtils")
 
 ---@class FileSystem
-local FileSystem = { author = "judged" }
-local DIR_SEP = package.config:sub(1,1)
-
+local FileSystem = { author = "judged", DIR_SEP = package.config:sub(1,1) }
 
 --- Find a file based on the root directory
 ---@param root string: absolute file path to root folder to search from
@@ -13,8 +12,8 @@ local DIR_SEP = package.config:sub(1,1)
 ---@return string: file path of found file, empty string if not
 function FileSystem.FindFile(root, fileName, isRecursive)
 	for entity in lfs.dir(root) do
-		if entity ~= "." and entity ~= ".." then
-			local fullPath = root..DIR_SEP..entity
+		if entity ~= "." and entity ~= ".." and entity ~= ".git" and entity ~= ".vscode" then
+            local fullPath = FileSystem.PathJoin(root, entity)
 			local mode = lfs.attributes(fullPath, "mode")
 			if mode == "file" and string.lower(entity) == string.lower(fileName) then
 				return fullPath
@@ -34,6 +33,33 @@ end
 function FileSystem.FileExists(filePath)
     local mode = lfs.attributes(filePath, "mode")
     return mode == "file"
+end
+
+---Opens file, reads it to a single string, and closes file
+---@param filePath string
+---@return string
+function FileSystem.ReadFile(filePath)
+    assert(FileSystem.FileExists(filePath), "Unable to find file: " .. filePath)
+    local file = io.open(filePath, "r")
+    assert(file ~= nil, "Unable to open file: " .. filePath)
+    local result = file:read("*a")
+    file:close()
+    return result
+end
+
+---Opens or Creates file, writes content into it, then closes file
+---@param filePath string
+---@param content string
+function FileSystem.WriteFile(filePath, content)
+    local file = io.open(filePath, "w+")
+    assert(file ~= nil, "Unable to create file: " .. filePath)
+    file:write(content)
+    file:close()
+end
+
+---@return string
+function FileSystem.PathJoin(...)
+    return StringUtils.Join({...}, FileSystem.DIR_SEP)
 end
 
 return FileSystem
