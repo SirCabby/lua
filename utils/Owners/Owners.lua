7f32b127d5cbd2ebd8d81264abb16833319e3348
SirCabby <77202713+SirCabby@users.noreply.github.com>
2022-10-03 15:39:59 -0400

    added owners

---
+++ utils/Owners/Owners.lua
@@ -0,0 +1,56 @@
+local Config = require("utils.Config.Config")
+local TableUtils = require("utils.TableUtils.TableUtils")
+
+---@class Owners
+---@field store table array of owners
+local Owners = { author = "judged", debug = false, configKey = "Owners" }
+
+---@param configFilePath string
+---@return Owners
+function Owners:new(configFilePath)
+    local owners = {}
+    setmetatable(owners, self)
+    self.__index = self
+    local config = Config:new(configFilePath)
+
+    ---Adds a new owner
+    ---@param name string
+    function Owners:Add(name)
+        local ownersConfig = config:GetConfig(Owners.configKey)
+        if not TableUtils.IsArray(ownersConfig) then error("Owners config was not an array") end
+        if not TableUtils.ArrayContains(ownersConfig, name) then
+            ownersConfig[#ownersConfig + 1] = name
+            print("Addded [" .. name .. "] as Owner")
+        end
+        Config:SaveConfig(Owners.configKey, ownersConfig)
+    end
+
+    ---Removes a current owner
+    ---@param name string
+    function Owners:Remove(name)
+        local ownersConfig = config:GetConfig(Owners.configKey)
+        if not TableUtils.IsArray(ownersConfig) then error("Owners config was not an array") end
+        if TableUtils.ArrayContains(ownersConfig, name) then
+            TableUtils.RemoveByValue(ownersConfig, name)
+            print("Removed [" .. name .. "] as Owner")
+        end
+        Config:SaveConfig(Owners.configKey, ownersConfig)
+    end
+
+    ---Returns true if name is listed as an owner
+    ---@param name any
+    ---@return boolean
+    function Owners:IsOwner(name)
+        local ownersConfig = config:GetConfig(Owners.configKey)
+        return TableUtils.ArrayContains(ownersConfig, name)
+    end
+
+    function Owners:Print()
+        local ownersConfig = config:GetConfig(Owners.configKey)
+        TableUtils.Print(ownersConfig)
+    end
+
+    return owners
+end
+
+return Owners
---
+++ utils/Owners/testOwners.lua
@@ -0,0 +1,20 @@
+local mq = require("mq")
+local FileSystem = require("utils.FileSystem")
+local Owners = require("utils.Owners.Owners")
+
+mq.cmd("/mqclear")
+
+local filePath = FileSystem.PathJoin(mq.configDir, "test", "ownersTest.json")
+local owners = Owners:new(filePath)
+
+owners:Print()
+owners:Add("Test Owner 1")
+owners:Print()
+owners:Add("Test Owner 1")
+owners:Add("Test Owner 2")
+owners:Print()
+print("is foo an owner? " .. tostring(owners:IsOwner("foo")))
+print("is [test owner 2] an owner? " .. tostring(owners:IsOwner("Test Owner 2")))
+owners:Remove("Test Owner 2")
+owners:Print()
+print("is [Test Owner 2] an owner? " .. tostring(owners:IsOwner("Test Owner 2")))
