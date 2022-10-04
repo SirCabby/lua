a463d620a69aa4244b31b530d9cfcba2a3ccc3ad
SirCabby <77202713+SirCabby@users.noreply.github.com>
2022-10-04 15:05:11 -0400

    Create priorities.lua

---
+++ cabby/priorities.lua
@@ -0,0 +1,25 @@
+
+local Priorities = {
+    Immediate = 0,
+    Combat = 79,
+    Following = 109,
+    Misc = 129
+}
+
+return Priorities
+
+-- | 1 My commands / Task / DZ
+-- | 19 Passive Mode
+-- | 29 Cure
+-- | 39 Heal
+-- | 49 Pulling
+-- | -- IN COMBAT --
+-- | 59 Mez
+-- | 69 Tank / grab aggro
+-- | 79 Dps
+-- | -- OUT COMBAT
+-- | 89 Looting
+-- | 99 Anchor
+-- | 109 Following
+-- | 119 Buff
+-- | 129 Misc
