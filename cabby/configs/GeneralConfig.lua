5d52935be287517b2f0286789af52114439d5696
SirCabby <77202713+SirCabby@users.noreply.github.com>
2022-10-07 13:58:06 -0400

    Fix return bug and add relaytellsto

--- cabby/configs/GeneralConfig.lua
+++ cabby/configs/GeneralConfig.lua
@@ -1,8 +1,18 @@
 local Config = require("utils.Config.Config")
 local TableUtils = require("utils.TableUtils.TableUtils")
 
-local GeneralConfig = { debug = false, configKey = "General" }
+---@class GeneralConfig
+local GeneralConfig = {
+    debug = false,
+    configKey = "General",
+    keys = {
+        version = "version",
+        relayTellsTo = "relayTellsTo"
+    }
+}
 
+---@param configFilePath string
+---@return GeneralConfig
 function GeneralConfig:new(configFilePath)
     local generalConfig = {}
     setmetatable(generalConfig, self)
@@ -14,19 +24,31 @@ function GeneralConfig:new(configFilePath)
         if GeneralConfig.debug then print(str) end
     end
 
+    function GeneralConfig:GetRelayTellsTo()
+        local generalConfig = config:GetConfig(GeneralConfig.configKey)
+        return generalConfig[GeneralConfig.keys.relayTellsTo]
+    end
+
+    function GeneralConfig:SetRelayTellsTo(name)
+        local generalConfig = config:GetConfig(GeneralConfig.configKey)
+        Debug("Set relayTellsTo: [" .. name .. "]")
+        generalConfig[GeneralConfig.keys.relayTellsTo] = name
+        config:SaveConfig(GeneralConfig.configKey, generalConfig)
+    end
+
     function GeneralConfig:Print()
         local generalConfig = config:GetConfig(GeneralConfig.configKey)
         TableUtils.Print(generalConfig)
     end
 
-    local generalConfig = config:GetConfig(GeneralConfig.configKey)
+    local configForGeneral = config:GetConfig(GeneralConfig.configKey)
     local taint = false
-    if generalConfig["version"] == nil then
+    if configForGeneral[GeneralConfig.keys.version] == nil then
         Debug("General Version was not set, updating...")
-        generalConfig["version"] = 1
+        configForGeneral[GeneralConfig.keys.version] = 1
         taint = true
     end
-    if taint then config:SaveConfig(GeneralConfig.configKey, generalConfig) end
+    if taint then config:SaveConfig(GeneralConfig.configKey, configForGeneral) end
 
     return generalConfig
 end
