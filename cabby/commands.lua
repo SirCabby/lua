fd85f11827f9e75365de0ef083c7940f9b3f4602
SirCabby <77202713+SirCabby@users.noreply.github.com>
2023-07-10 23:33:40 -0400

    added groupInvite event

--- cabby/commands.lua
+++ cabby/commands.lua
@@ -1,5 +1,6 @@
 local mq = require("mq")
 local Config = require("utils.Config.Config")
+local Debug = require("utils.Debug.Debug")
 local GeneralConfig = require("cabby.configs.GeneralConfig")
 local Owners = require("utils.Owners.Owners")
 local Priorities = require("cabby.priorities")
@@ -8,13 +9,13 @@ local TableUtils = require("utils.TableUtils.TableUtils")
 
 ---@class Commands
 local Commands = {
-    debug = false,
-    configKey = "Commands",
+    key = "Commands",
     eventIds = {
         followMe = "Follow Me",
         stopFollow = "Stop Follow",
         moveToMe = "Move to Me",
-        tellToMe = "Tell to Me"
+        tellToMe = "Tell to Me",
+        groupInvited = "Invited to Group"
     },
     channelTypes = {
         bc = "bc",
@@ -35,9 +36,10 @@ function Commands:new(configFilePath, priorityQueue)
     commands.priorityQueue = priorityQueue
     local config = Config:new(configFilePath)
     local owners = Owners:new(configFilePath)
+    Debug:new()
 
-    local function Debug(str)
-        if Commands.debug then print(str) end
+    local function DebugLog(str)
+        Debug:Log(Commands.key, str)
     end
 
     ---Decides if should listen to command, then schedules it in the priority queue
@@ -48,18 +50,18 @@ function Commands:new(configFilePath, priorityQueue)
     ---@return boolean - true if command scheduled, false if ignored
     local function ScheduleCommand(speaker, priority, command, who)
         who = who or "all"
-        Debug("Received command from [" .. speaker .. "] with priority [" .. tostring(priority) .. "] directed at: [" .. who .. "]: " .. command.identity)
+        DebugLog("Received command from [" .. speaker .. "] with priority [" .. tostring(priority) .. "] directed at: [" .. who .. "]: " .. command.identity)
         if who ~= "all" and who:lower() ~= mq.TLO.Me.Name():lower() then
-            Debug("Ignoring command, was issued to: [" .. mq.TLO.Me.Name() .. "]")
+            DebugLog("Ignoring command, was issued to: [" .. mq.TLO.Me.Name() .. "]")
             return false
         end
 
         if not Owners:IsOwner(speaker) then
-            Debug("Ignoring command, speaker was not an owner [" .. speaker .. "]")
+            DebugLog("Ignoring command, speaker was not an owner [" .. speaker .. "]")
             return false
         end
 
-        Debug("Inserting command: " .. command.identity)
+        DebugLog("Inserting command: " .. command.identity)
         commands.priorityQueue:InsertNewJob(priority, command)
         return true
     end
@@ -70,7 +72,7 @@ function Commands:new(configFilePath, priorityQueue)
     ---@param eventFunction function
     local function AddEventChannels(eventId, phrase, eventFunction)
         mq.unevent(eventId)
-        local commandsConfig = config:GetConfig(Commands.configKey)
+        local commandsConfig = config:GetConfig(Commands.key)
         local channels = commandsConfig.Channels or {}
         if TableUtils.ArrayContains(channels, Commands.channelTypes.bc) then
             mq.event(eventId, "<#1#> " .. phrase, eventFunction)
@@ -91,14 +93,18 @@ function Commands:new(configFilePath, priorityQueue)
     -----------------------------------------------------------------------------
     ------------------------------- COMMS ---------------------------------------
     -----------------------------------------------------------------------------
+    local function exeFunc()
+        print("insert follow me here")
+        return true;
+    end
 
     local function event_FollowMe(_, speaker, who)
-        local followMe = PriorityQueueFunctionContent:new("Follow Me - " .. speaker, function() return print("insert follow me here") end)
+        local followMe = PriorityQueueFunctionContent:new("Follow Me - " .. speaker, exeFunc)
         ScheduleCommand(speaker, Priorities.Following, followMe, who)
     end
 
     local function event_StopFollow(_, speaker, who)
-        local stopFollow = PriorityQueueFunctionContent:new("Stop Follow - " .. speaker, function() return print("insert follow me here") end)
+        local stopFollow = PriorityQueueFunctionContent:new("Stop Follow - " .. speaker, exeFunc)
         ScheduleCommand(speaker, Priorities.Following, stopFollow, who)
     end
 
@@ -108,19 +114,35 @@ function Commands:new(configFilePath, priorityQueue)
     ------------------------------ EVENTS ---------------------------------------
     -----------------------------------------------------------------------------
 
-    local function tellToMe(speaker, message)
-        local generalConfig = GeneralConfig:new(configFilePath)
-        local tellTo = generalConfig:GetRelayTellsTo()
-        if tellTo ~= speaker and mq.TLO.SpawnCount("npc speaker")() < 1 then
-            mq.cmd("/tell " .. tellTo .. " " .. speaker .. " told me: " .. message)
-        end
-        return true
-    end
     local function event_TellToMe(_, speaker, message)
+        local function tellToMe(speaker, message)
+            local generalConfig = GeneralConfig:new(configFilePath)
+            local tellTo = generalConfig:GetRelayTellsTo()
+            if tellTo ~= speaker and mq.TLO.SpawnCount("npc " .. speaker)() < 1 then
+                mq.cmd("/tell " .. tellTo .. " " .. speaker .. " told me: " .. message)
+            end
+            return true
+        end
         local tellToMeFunc = PriorityQueueFunctionContent:new("Tell to me - " .. speaker, function() return tellToMe(speaker, message) end)
         commands.priorityQueue:InsertNewJob(Priorities.Immediate, tellToMeFunc)
     end
     mq.event(Commands.eventIds.tellToMe, "#1# tells you, '#2#'", event_TellToMe)
+
+    local function event_GroupInvited(_, speaker)
+        local function groupInvited(speaker)
+            if Owners:IsOwner(speaker) then
+                DebugLog("Joining group of speaker [" .. speaker .. "]")
+                mq.cmd("/invite")
+            else
+                DebugLog("Declining group of speaker [" .. speaker .. "]")
+                mq.cmd("/disband")
+            end
+            return true
+        end
+        local groupInvitedFunc = PriorityQueueFunctionContent:new("Tell to me - " .. speaker, function() return groupInvited(speaker) end)
+        commands.priorityQueue:InsertNewJob(Priorities.Immediate, groupInvitedFunc)
+    end
+    mq.event(Commands.eventIds.groupInvited, "#1# invites you to join a group.", event_GroupInvited)
 
     -----------------------------------------------------------------------------
     ---------------------------- END EVENTS -------------------------------------
@@ -130,23 +152,30 @@ function Commands:new(configFilePath, priorityQueue)
 
     local function chelpPrint()
         print("(/chelp) Cabby Help menu")
-        print(" -- Pick a help topic. Options: [Comms, Commands]")
+        print(" -- Pick a help topic. Options: [Cvc, Debug, Owners, Follow]")
     end
     local function Bind_Chelp(...)
         local args = {...} or {}
         if args == nil or #args < 1 or args[1]:lower() == "help" then
             chelpPrint()
-        elseif args[1]:lower() == "comms" then
-            print("(/chelp comms) Leverage these by speaking in active channels")
+        elseif args[1]:lower() == "cvc" then
+            print("(/chelp cvc) Explanation of Comms vs Slash Commands")
+            print("Comms are leveraged by speaking in active channels for other listeners to pick up")
             print(" -- For example: /bc followme")
             print(" -- To see active channels, use /activechannels")
             print(" -- To add / remove channels, use /addchannel name, /removechannel name")
-            print("Command list:")
+            print("Commands begin with a slash and are invoked by using the slash command on this char")
+            print(" -- For example: /activechannels")
+        elseif args[1]:lower() == "debug" then
+            print("(/chelp debug) Debug Commands")
+            print(" -- /debug [all|follow]")
+        elseif args[1]:lower() == "follow" then
+            print("(/chelp follow) Follow Commands:")
+            print(" -- See also: /chelp cvc")
             print(" -- followme")
             print(" -- stopfollow")
-        elseif args[1]:lower() == "commands" then
-            print("(/chelp commands) Slash Commands:")
-            print(" -- /chelp")
+        elseif args[1]:lower() == "owners" then
+            print("(/chelp owners) Owner Commands:")
             print(" -- /addowner")
             print(" -- /removeowner")
             print(" -- /showowners")
@@ -219,33 +248,33 @@ function Commands:new(configFilePath, priorityQueue)
     ---Available types found in Commands.channelTypes
     ---@param channel string
     function Commands:Add(channel)
-        local commandsConfig = config:GetConfig(Commands.configKey)
+        local commandsConfig = config:GetConfig(Commands.key)
         if not TableUtils.IsArray(commandsConfig.Channels) then error("Command.Channels config was not an array") end
         if not TableUtils.ArrayContains(commandsConfig.Channels, channel) then
             commandsConfig.Channels[#commandsConfig.Channels + 1] = channel
             print("Added [" .. channel .. "] to active channels")
-            Config:SaveConfig(Commands.configKey, commandsConfig)
+            Config:SaveConfig(Commands.key, commandsConfig)
             RegisterAllComms()
             return
         end
-        Debug(channel .. " was already an active channel")
+        DebugLog(channel .. " was already an active channel")
     end
 
     function Commands:Remove(channel)
-        local commandsConfig = config:GetConfig(Commands.configKey)
+        local commandsConfig = config:GetConfig(Commands.key)
         if not TableUtils.IsArray(commandsConfig.Channels) then error("Command.Channels config was not an array") end
         if TableUtils.ArrayContains(commandsConfig.Channels, channel) then
             TableUtils.RemoveByValue(commandsConfig, channel)
             print("Removed [" .. channel .. "] as active channel")
-            Config:SaveConfig(Commands.configKey, commandsConfig)
+            Config:SaveConfig(Commands.key, commandsConfig)
             RegisterAllComms()
             return
         end
-        Debug(channel .. " was not an active channel")
+        DebugLog(channel .. " was not an active channel")
     end
 
     function Commands:Print()
-        local commandsConfig = config:GetConfig(Commands.configKey)
+        local commandsConfig = config:GetConfig(Commands.key)
         TableUtils.Print(commandsConfig)
     end
 
