73dab75428515d808df334146bddcc447a854812
SirCabby <77202713+SirCabby@users.noreply.github.com>
2023-07-14 14:42:24 -0400

    refactor ownership of logic

--- cabby/commands.lua
+++ cabby/commands.lua
@@ -1,30 +1,18 @@
 local mq = require("mq")
-local Config = require("utils.Config.Config")
 local Debug = require("utils.Debug.Debug")
 local DebugConfig = require("cabby.configs.DebugConfig")
 local GeneralConfig = require("cabby.configs.GeneralConfig")
 local Owners = require("utils.Owners.Owners")
 local Priorities = require("cabby.priorities")
 local PriorityQueueFunctionContent = require("utils.PriorityQueue.PriorityQueueFunctionContent")
+local StringUtils = require("utils.StringUtils.StringUtils")
 local TableUtils = require("utils.TableUtils.TableUtils")
 
 ---@class Commands
 local Commands = {
     key = "Commands",
-    eventIds = {
-        followMe = "Follow Me",
-        stopFollow = "Stop Follow",
-        moveToMe = "Move to Me",
-        tellToMe = "Tell to Me",
-        groupInvited = "Invited to Group"
-    },
-    channelTypes = {
-        bc = "bc",
-        tell = "tell",
-        raid = "raid",
-        group = "group"
-    },
-    eventsRegistered = false
+    _registeredComms = {},
+    _registeredSlashCommands = {}
 }
 
 ---comment
@@ -34,14 +22,27 @@ function Commands:new(configFilePath, priorityQueue)
     local commands = {}
     setmetatable(commands, self)
     self.__index = self
+
     commands.priorityQueue = priorityQueue
-    local config = Config:new(configFilePath)
-    local owners = Owners:new(configFilePath)
     local debugConfig = DebugConfig:new(configFilePath)
+    local owners = Owners:new(configFilePath)
     Debug:new()
 
     local function DebugLog(str)
         Debug:Log(Commands.key, str)
+    end
+
+    ---@param command string
+    ---@param callback function
+    local function RegisterSlashCommand(command, callback)
+        command = command:lower()
+        if command:sub(1, 1) ~= "/" then
+            command = "/" .. command
+        end
+
+        mq.bind(command, callback)
+        Commands._registeredSlashCommands[#Commands._registeredSlashCommands + 1] = command
+        table.sort(Commands._registeredSlashCommands)
     end
 
     ---Decides if should listen to command, then schedules it in the priority queue
@@ -68,37 +69,9 @@ function Commands:new(configFilePath, priorityQueue)
         return true
     end
 
-    ---Adds this event to all registered channels
-    ---@param eventId string
-    ---@param phrase string
-    ---@param eventFunction function
-    local function AddEventChannels(eventId, phrase, eventFunction)
-        mq.unevent(eventId)
-        local commandsConfig = config:GetConfig(Commands.key)
-        local channels = commandsConfig.Channels or {}
-        if TableUtils.ArrayContains(channels, Commands.channelTypes.bc) then
-            mq.event(eventId, "<#1#> " .. phrase, eventFunction)
-            mq.event(eventId, "<#1#> #2# " .. phrase, eventFunction)
-            mq.event(eventId, "[#1#(msg)] " .. phrase, eventFunction)
-        end
-        if TableUtils.ArrayContains(channels, Commands.channelTypes.tell) then
-            mq.event(eventId, "<#1#> tells you, '" .. phrase .. "'", eventFunction)
-        end
-        if TableUtils.ArrayContains(channels, Commands.channelTypes.group) then
-            mq.event(eventId, "<#1#> tells the group, '" .. phrase .. "'", eventFunction)
-        end
-        if TableUtils.ArrayContains(channels, Commands.channelTypes.raid) then
-            mq.event(eventId, "<#1#> tells the raid, '" .. phrase .. "'", eventFunction)
-        end
-    end
-
     -----------------------------------------------------------------------------
     ------------------------------- COMMS ---------------------------------------
     -----------------------------------------------------------------------------
-    local function exeFunc()
-        print("insert follow me here")
-        return true;
-    end
 
     local function event_FollowMe(_, speaker, who)
         local followMe = PriorityQueueFunctionContent:new("Follow Me - " .. speaker, exeFunc)
@@ -110,6 +83,13 @@ function Commands:new(configFilePath, priorityQueue)
         ScheduleCommand(speaker, Priorities.Following, stopFollow, who)
     end
 
+    --TODO move these into follow state and setup method for registering state files
+
+    local function RegisterAllComms()
+        --GeneralConfig:UpdateEventChannels(FollowState.eventIds.followMe, "followme", event_FollowMe)
+        --GeneralConfig:UpdateEventChannels(FollowState.eventIds.stopFollow, "stopfollow", event_StopFollow)
+    end
+
     -----------------------------------------------------------------------------
     ----------------------------- END COMMS -------------------------------------
     -----------------------------------------------------------------------------
@@ -117,34 +97,16 @@ function Commands:new(configFilePath, priorityQueue)
     -----------------------------------------------------------------------------
 
     local function event_TellToMe(_, speaker, message)
-        local function tellToMe(speaker, message)
-            local generalConfig = GeneralConfig:new(configFilePath)
-            local tellTo = generalConfig:GetRelayTellsTo()
-            if tellTo ~= speaker and mq.TLO.SpawnCount("npc " .. speaker)() < 1 then
-                mq.cmd("/tell " .. tellTo .. " " .. speaker .. " told me: " .. message)
-            end
-            return true
-        end
-        local tellToMeFunc = PriorityQueueFunctionContent:new("Tell to me - " .. speaker, function() return tellToMe(speaker, message) end)
+        local tellToMeFunc = PriorityQueueFunctionContent:new("Tell to me - " .. speaker, function() return GeneralConfig:TellToMe(speaker, message) end)
         commands.priorityQueue:InsertNewJob(Priorities.Immediate, tellToMeFunc)
     end
-    mq.event(Commands.eventIds.tellToMe, "#1# tells you, '#2#'", event_TellToMe)
+    mq.event(GeneralConfig.eventIds.tellToMe, "#1# tells you, '#2#'", event_TellToMe)
 
     local function event_GroupInvited(_, speaker)
-        local function groupInvited(speaker)
-            if Owners:IsOwner(speaker) then
-                DebugLog("Joining group of speaker [" .. speaker .. "]")
-                mq.cmd("/invite")
-            else
-                DebugLog("Declining group of speaker [" .. speaker .. "]")
-                mq.cmd("/disband")
-            end
-            return true
-        end
-        local groupInvitedFunc = PriorityQueueFunctionContent:new("Tell to me - " .. speaker, function() return groupInvited(speaker) end)
+        local groupInvitedFunc = PriorityQueueFunctionContent:new("Tell to me - " .. speaker, function() return GeneralConfig:GroupInvited(speaker) end)
         commands.priorityQueue:InsertNewJob(Priorities.Immediate, groupInvitedFunc)
     end
-    mq.event(Commands.eventIds.groupInvited, "#1# invites you to join a group.", event_GroupInvited)
+    mq.event(GeneralConfig.eventIds.groupInvited, "#1# invites you to join a group.", event_GroupInvited)
 
     -----------------------------------------------------------------------------
     ---------------------------- END EVENTS -------------------------------------
@@ -154,77 +116,74 @@ function Commands:new(configFilePath, priorityQueue)
 
     local function chelpPrint()
         print("(/chelp) Cabby Help menu")
-        print(" -- Pick a help topic. Options: [Cvc, Debug, Owners, Follow]")
+        print(" -- Pick a help topic. Options: [Cvc, SlashCmds, Comms]")
+        print("Additional options include any registered Comm or Slash Command listed in SlashCmds or Comms")
+        print(" -- Example: /chelp activechannels")
+        print("To learn more about the differences between SlashCmds vs Comms, use /chelp cvc")
     end
     local function Bind_Chelp(...)
         local args = {...} or {}
         if args == nil or #args < 1 or args[1]:lower() == "help" then
             chelpPrint()
-        elseif args[1]:lower() == "cvc" then
-            print("(/chelp cvc) Explanation of Comms vs Slash Commands")
-            print("Comms are leveraged by speaking in active channels for other listeners to pick up")
-            print(" -- For example: /bc followme")
-            print(" -- To see active channels, use /activechannels")
-            print(" -- To add / remove channels, use /addchannel name, /removechannel name")
-            print("Commands begin with a slash and are invoked by using the slash command on this char")
-            print(" -- For example: /activechannels")
-        elseif args[1]:lower() == "debug" then
-            print("(/chelp debug) Debug Commands")
-            print(" -- To enable all debug tracing, use /debug all")
-            print(" -- To see a list of currently used debug toggles, use /debug list")
-            print(" -- To toggle a particular debug trace, find a key in the list command, then use /debug <key>")
-        elseif args[1]:lower() == "follow" then
-            print("(/chelp follow) Follow Commands:")
-            print(" -- See also: /chelp cvc")
-            print(" -- followme")
-            print(" -- stopfollow")
-        elseif args[1]:lower() == "owners" then
-            print("(/chelp owners) Owner Commands:")
-            print(" -- /addowner")
-            print(" -- /removeowner")
-            print(" -- /showowners")
         else
-            chelpPrint()
+            arg = args[1]:lower()
+            if arg == "cvc" then
+                print("(/chelp cvc) Explanation of Comms vs Slash Commands:")
+                print("Comms (Communications) are leveraged by speaking in active channels for other listeners to pick up")
+                print(" -- /<channel> <command>, For example: /bc followme")
+                print(" -- To manage active channels, use /activechannels")
+                print(" -- To see all registered communication commands provided by this script, use /chelp comms")
+                print("Slash Commands begin with a slash and are invoked by using the slash command on this char")
+                print(" -- For example: /activechannels")
+                print(" -- To see all registered slash commands provided by this script, use /chelp slashcmds")
+            elseif arg == "comms" then
+                print("Available Communication Commands: [" .. StringUtils.Join(Commands._registeredComms, ", ") .. "]")
+            elseif arg == "slashcmds" then
+                print("Available Slash Commands: [" .. StringUtils.Join(Commands._registeredSlashCommands, ", ") .. "]")
+            elseif TableUtils.ArrayContains(Commands._registeredSlashCommands, "/" .. arg) then
+                mq.cmd("/" .. arg .. " help")
+            else
+                chelpPrint()
+            end
         end
     end
-    mq.bind("/chelp", Bind_Chelp)
+    RegisterSlashCommand("chelp", Bind_Chelp)
 
     -----------------------------------------------------------------------------
 
-    local function addOwner(...)
+    local function Bind_ActiveChannels(...)
         local args = {...} or {}
-        if args == nil or #args < 1 or args[1]:lower() == "help" then
-            print("(/addowner) Adds Owners to listen to")
-            print(" -- Usage: /addowner name")
+        if args == nil or #args ~= 1 or args[1]:lower() == "help" then
+            print("(/activechannels) Channels used for listening to commands")
+            print("To toggle an active channel, use: /activechannels channel")
+            print("Valid Active Channel Types: [" .. StringUtils.Join(TableUtils.GetValues(GeneralConfig.channelTypes), ", ") .. "]")
+            print("Currently active channels: [" .. StringUtils.Join(GeneralConfig:GetActiveChannels(), ", ") .. "]")
+        elseif GeneralConfig:ToggleActiveChannel(args[1]:lower()) then
+            RegisterAllComms()
+        end
+    end
+    RegisterSlashCommand("activechannels", Bind_ActiveChannels)
+
+    -----------------------------------------------------------------------------
+
+    local function Bind_Owners(...)
+        local args = {...} or {}
+        if args == nil or #args ~= 1 or args[1]:lower() == "help" then
+            print("(/owners) Manage owners to take commands from")
+            print("To add/remove owners, use: /owners name")
+            print("Current owners:")
+            owners:Print()
+        elseif owners:IsOwner(args[1]) then
+            owners:Remove(args[1])
         else
             owners:Add(args[1])
         end
     end
-    mq.bind("/addowner", addOwner)
+    RegisterSlashCommand("owners", Bind_Owners)
 
     -----------------------------------------------------------------------------
 
-    local function removeOwner(...)
-        local args = {...} or {}
-        if args == nil or #args < 1 or args[1]:lower() == "help" then
-            print("(/removeowner) Removes Owners to listen to")
-            print(" -- Usage: /removeowner name")
-        else
-            owners:Remove(args[1])
-        end
-    end
-    mq.bind("/removeowner", removeOwner)
-
-    -----------------------------------------------------------------------------
-
-    local function showOwners(...)
-        owners:Print()
-    end
-    mq.bind("/showowners", showOwners)
-
-    -----------------------------------------------------------------------------
-
-    local function setRelayTellsToFunc(...)
+    local function Bind_SetRelayTellsToFunc(...)
         local args = {...} or {}
         if args == nil or #args < 1 or args[1]:lower() == "help" then
             print("(/relaytellsto) Relays tells received to this character")
@@ -235,11 +194,11 @@ function Commands:new(configFilePath, priorityQueue)
             print("Relaying future tells to: [" .. generalConfig:GetRelayTellsTo() .. "]")
         end
     end
-    mq.bind("/relaytellsto", setRelayTellsToFunc)
+    RegisterSlashCommand("relaytellsto", Bind_SetRelayTellsToFunc)
 
     -----------------------------------------------------------------------------
 
-    local function debugCommand(...)
+    local function Bind_Debug(...)
         local args = {...} or {}
         if args == nil or #args < 1 or #args > 2 or args[1]:lower() == "help" then
             print("(/debug) Toggle debug tracing by debug category key")
@@ -265,52 +224,11 @@ function Commands:new(configFilePath, priorityQueue)
             print("Toggled debug tracing for [" .. args[1] .. "]: " .. tostring(DebugConfig:GetDebugToggle(args[1])))
         end
     end
-    mq.bind("/debug", debugCommand)
+    RegisterSlashCommand("debug", Bind_Debug)
 
     -----------------------------------------------------------------------------
     ----------------------------- END BINDS -------------------------------------
     -----------------------------------------------------------------------------
-
-    local function RegisterAllComms()
-        if Commands.eventsRegistered then return end
-        Commands.eventsRegistered = true
-        AddEventChannels(Commands.eventIds.followMe, "followme", event_FollowMe)
-        AddEventChannels(Commands.eventIds.stopFollow, "stopfollow", event_StopFollow)
-    end
-
-    ---Adds a new event channel to listen to
-    ---Available types found in Commands.channelTypes
-    ---@param channel string
-    function Commands:Add(channel)
-        local commandsConfig = config:GetConfig(Commands.key)
-        if not TableUtils.IsArray(commandsConfig.Channels) then error("Command.Channels config was not an array") end
-        if not TableUtils.ArrayContains(commandsConfig.Channels, channel) then
-            commandsConfig.Channels[#commandsConfig.Channels + 1] = channel
-            print("Added [" .. channel .. "] to active channels")
-            Config:SaveConfig(Commands.key, commandsConfig)
-            RegisterAllComms()
-            return
-        end
-        DebugLog(channel .. " was already an active channel")
-    end
-
-    function Commands:Remove(channel)
-        local commandsConfig = config:GetConfig(Commands.key)
-        if not TableUtils.IsArray(commandsConfig.Channels) then error("Command.Channels config was not an array") end
-        if TableUtils.ArrayContains(commandsConfig.Channels, channel) then
-            TableUtils.RemoveByValue(commandsConfig, channel)
-            print("Removed [" .. channel .. "] as active channel")
-            Config:SaveConfig(Commands.key, commandsConfig)
-            RegisterAllComms()
-            return
-        end
-        DebugLog(channel .. " was not an active channel")
-    end
-
-    function Commands:Print()
-        local commandsConfig = config:GetConfig(Commands.key)
-        TableUtils.Print(commandsConfig)
-    end
 
     RegisterAllComms()
 
--- cabby/configs/GeneralConfig.lua
+++ cabby/configs/GeneralConfig.lua
@@ -1,5 +1,8 @@
+local mq = require("mq")
 local Config = require("utils.Config.Config")
 local Debug = require("utils.Debug.Debug")
+local Owners = require("utils.Owners.Owners")
+local StringUtils = require("utils.StringUtils.StringUtils")
 local TableUtils = require("utils.TableUtils.TableUtils")
 
 ---@class GeneralConfig
@@ -7,7 +10,18 @@ local GeneralConfig = {
     key = "General",
     keys = {
         version = "version",
-        relayTellsTo = "relayTellsTo"
+        relayTellsTo = "relayTellsTo",
+        activeChannels = "activeChannels"
+    },
+    channelTypes = {
+        bc = "bc",
+        tell = "tell",
+        raid = "raid",
+        group = "group"
+    },
+    eventIds = {
+        tellToMe = "Tell to Me",
+        groupInvited = "Invited to Group"
     }
 }
 
@@ -17,8 +31,10 @@ function GeneralConfig:new(configFilePath)
     local generalConfig = {}
     setmetatable(generalConfig, self)
     self.__index = self
+
     local config = Config:new(configFilePath)
     Debug:new()
+    Owners:new(configFilePath)
 
     ---@param str string
     local function DebugLog(str)
@@ -37,11 +53,121 @@ function GeneralConfig:new(configFilePath)
         config:SaveConfig(GeneralConfig.key, generalConfig)
     end
 
+    function GeneralConfig:TellToMe(speaker, message)
+        local generalConfig = GeneralConfig:new(configFilePath)
+        local tellTo = generalConfig:GetRelayTellsTo()
+        if tellTo ~= speaker and mq.TLO.SpawnCount("npc " .. speaker)() < 1 then
+            mq.cmd("/tell " .. tellTo .. " " .. speaker .. " told me: " .. message)
+        end
+        return true
+    end
+
+    function GeneralConfig:GroupInvited(speaker)
+        if Owners:IsOwner(speaker) then
+            DebugLog("Joining group of speaker [" .. speaker .. "]")
+            mq.cmd("/invite")
+        else
+            DebugLog("Declining group of speaker [" .. speaker .. "]")
+            mq.cmd("/disband")
+        end
+    end
+
+    ---@return array
+    function GeneralConfig:GetActiveChannels()
+        local generalConfig = config:GetConfig(GeneralConfig.key)
+        return generalConfig[GeneralConfig.keys.activeChannels]
+    end
+
+    ---Toggles an active command channel
+    ---@param channel string Available types found in GeneralConfig.channelTypes
+    ---@return boolean result - true if need to refresh channels, false if nothing changed
+    function GeneralConfig:ToggleActiveChannel(channel)
+        local generalConfig = config:GetConfig(GeneralConfig.key) or {}
+        if not TableUtils.ArrayContains(TableUtils.GetValues(GeneralConfig.channelTypes), channel) then
+            print("Invalid Channel Type. Valid Channel Types: [" .. StringUtils.Join(TableUtils.GetValues(GeneralConfig.channelTypes), ", ") .. "]")
+            return false
+        end
+        if TableUtils.ArrayContains(generalConfig[GeneralConfig.keys.activeChannels], channel) then
+            TableUtils.RemoveByValue(generalConfig[GeneralConfig.keys.activeChannels], channel)
+            print("Removed [" .. channel .. "] as active channel")
+        else
+            generalConfig[GeneralConfig.keys.activeChannels][#generalConfig[GeneralConfig.keys.activeChannels] + 1] = channel
+            print("Added [" .. channel .. "] to active channels")
+        end
+        Config:SaveConfig(GeneralConfig.key, generalConfig)
+        return true
+    end
+
+    ---Adds an active command channel
+    ---@param channel string Available types found in GeneralConfig.channelTypes
+    ---@return boolean result true if need to refresh channels, false if nothing changed
+    function GeneralConfig:AddChannel(channel)
+        local generalConfig = config:GetConfig(GeneralConfig.key) or {}
+        if not TableUtils.IsArray(generalConfig[GeneralConfig.keys.activeChannels]) then error("GeneralConfig.Channels config was not an array") end
+        if not TableUtils.ArrayContains(TableUtils.GetValues(GeneralConfig.channelTypes), channel) then
+            print("Invalid Channel Type. Valid Channel Types: [" .. StringUtils.Join(TableUtils.GetValues(GeneralConfig.channelTypes), ", ") .. "]")
+            return false
+        end
+        if not TableUtils.ArrayContains(generalConfig[GeneralConfig.keys.activeChannels], channel) then
+            generalConfig[GeneralConfig.keys.activeChannels][#generalConfig[GeneralConfig.keys.activeChannels] + 1] = channel
+            print("Added [" .. channel .. "] to active channels")
+            Config:SaveConfig(GeneralConfig.key, generalConfig)
+            return true
+        end
+        DebugLog(channel .. " was already an active channel")
+        return false
+    end
+
+    ---Removes an active command channel
+    ---@param channel string Available types found in GeneralConfig.channelTypes
+    ---@return boolean result true if need to refresh channels, false if nothing changed
+    function GeneralConfig:RemoveChannel(channel)
+        local generalConfig = config:GetConfig(GeneralConfig.key) or {}
+        if not TableUtils.IsArray(generalConfig[GeneralConfig.keys.activeChannels]) then error("Command.Channels config was not an array") end
+        if not TableUtils.ArrayContains(TableUtils.GetValues(GeneralConfig.channelTypes), channel) then
+            print("Invalid Channel Type. Valid Channel Types: [" .. StringUtils.Join(TableUtils.GetValues(GeneralConfig.channelTypes), ", ") .. "]")
+            return false
+        end
+        if TableUtils.ArrayContains(generalConfig[GeneralConfig.keys.activeChannels], channel) then
+            TableUtils.RemoveByValue(generalConfig[GeneralConfig.keys.activeChannels], channel)
+            print("Removed [" .. channel .. "] as active channel")
+            Config:SaveConfig(GeneralConfig.key, generalConfig)
+            return true
+        end
+        DebugLog(channel .. " was not an active channel")
+        return false
+    end
+
     function GeneralConfig:Print()
         local generalConfig = config:GetConfig(GeneralConfig.key)
         TableUtils.Print(generalConfig)
     end
 
+    ---Removes and re-adds this event to all registered channels
+    ---@param eventId string
+    ---@param phrase string
+    ---@param eventFunction function
+    function GeneralConfig:UpdateEventChannels(eventId, phrase, eventFunction)
+        mq.unevent(eventId)
+        local generalConfig = config:GetConfig(GeneralConfig.key)
+        local channels = generalConfig.Channels or {}
+        if TableUtils.ArrayContains(channels, GeneralConfig.channelTypes.bc) then
+            mq.event(eventId, "<#1#> " .. phrase, eventFunction)
+            mq.event(eventId, "<#1#> #2# " .. phrase, eventFunction)
+            mq.event(eventId, "[#1#(msg)] " .. phrase, eventFunction)
+        end
+        if TableUtils.ArrayContains(channels, GeneralConfig.channelTypes.tell) then
+            mq.event(eventId, "<#1#> tells you, '" .. phrase .. "'", eventFunction)
+        end
+        if TableUtils.ArrayContains(channels, GeneralConfig.channelTypes.group) then
+            mq.event(eventId, "<#1#> tells the group, '" .. phrase .. "'", eventFunction)
+        end
+        if TableUtils.ArrayContains(channels, GeneralConfig.channelTypes.raid) then
+            mq.event(eventId, "<#1#> tells the raid, '" .. phrase .. "'", eventFunction)
+        end
+    end
+
+    -- Init any keys that were not setup
     local configForGeneral = config:GetConfig(GeneralConfig.key)
     local taint = false
     if configForGeneral[GeneralConfig.keys.version] == nil then
@@ -49,7 +175,19 @@ function GeneralConfig:new(configFilePath)
         configForGeneral[GeneralConfig.keys.version] = 1
         taint = true
     end
+    if configForGeneral[GeneralConfig.keys.activeChannels] == nil then
+        DebugLog("Active Channels were not set, updating...")
+        configForGeneral[GeneralConfig.keys.activeChannels] = {}
+        taint = true
+    end
     if taint then config:SaveConfig(GeneralConfig.key, configForGeneral) end
+
+    -- Validation reminders
+    if #configForGeneral[GeneralConfig.keys.activeChannels] < 1 then
+        print("Not currently listening on any active channels. To learn more, /chelp activechannels")
+    else
+        print("Currently listening on active channels: [" .. StringUtils.Join(GeneralConfig:GetActiveChannels(), ", ") .. "]")
+    end
 
     return generalConfig
 end
