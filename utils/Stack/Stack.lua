20c1fe66de58745e845d0e37bbb8d6db1c9e6b8a
SirCabby <77202713+SirCabby@users.noreply.github.com>
2023-07-16 17:27:21 -0400

    fix instancing of stacks

--- utils/Stack/Stack.lua
+++ utils/Stack/Stack.lua
@@ -1,42 +1,47 @@
 local Debug = require("utils.Debug.Debug")
 
 ---@class Stack
----@field stack table
 local Stack = { author = "judged", key = "Stack" }
 
-function Stack:new()
-    local stack = {}
-    setmetatable(stack, self)
-    self.__index = self
-    stack.stack = {}
-    Debug:new()
-    return stack
-end
-
-local function DebugLog(str)
-    Debug:Log(Stack.key, str)
-end
-
----@param obj any
+---@meta Stack
+---@param obj any nil will be converted to string "nil"
 ---@return Stack - self
-function Stack:push(obj)
-    DebugLog("Pushed to top of stack: " .. tostring(obj))
-    table.insert(self.stack, obj)
-    return self
-end
-
+function Stack:push(obj) end
 ---@param index? number 
 ---@return any
-function Stack:pop(index)
-    index = index or #self.stack
-    DebugLog("Popped from top of stack: " .. tostring(self.stack[index]))
-    return table.remove(self.stack, index)
-end
-
+function Stack:pop(index) end
 ---@return any 
-function Stack:peek()
-    DebugLog("Peeked from top of stack: " .. tostring(self.stack[#self.stack]))
-    return self.stack[#self.stack]
+function Stack:peek() end
+
+function Stack:new()
+    local stack = { _stack = {} }
+    Debug:new()
+
+    local function DebugLog(str)
+        Debug:Log(Stack.key, str)
+    end
+
+    function stack:push(obj)
+        if obj == nil then obj = "nil" end
+        DebugLog("Pushed to top of stack: " .. tostring(obj))
+        table.insert(stack._stack, obj)
+        return stack
+    end
+
+    function stack:pop(index)
+        index = index or #stack._stack
+        local popped = table.remove(stack._stack, index)
+        DebugLog("Popped from stack at index [" .. tostring(index) .. "]: " .. tostring(popped))
+        return popped
+    end
+
+    function stack:peek()
+        local peeked = stack._stack[#stack._stack]
+        DebugLog("Peeked from top of stack: " .. tostring(peeked))
+        return peeked
+    end
+
+    return stack
 end
 
 return Stack
