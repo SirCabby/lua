1f98f216c1e6cb56c8a17f0e822c339ffd5a5a7d
SirCabby <77202713+SirCabby@users.noreply.github.com>
2024-01-19 21:17:42 -0500

    Create userinput.lua

---
+++ cabby/utils/userinput.lua
@@ -0,0 +1,31 @@
+---@class UserInput
+local UserInput = { author = "judged", key = "UserInput" }
+
+---Return true for truthy inputs
+---@param inputStr string
+---@return boolean
+UserInput.IsTrue = function(inputStr)
+    inputStr = inputStr:lower()
+    return inputStr == "on" or
+        inputStr == "true" or
+        inputStr == "enable" or
+        inputStr == "yes" or
+        inputStr == "1"
+end
+
+---Return false for falsy inputs
+---@param inputStr string
+---@return boolean
+UserInput.IsFalse = function(inputStr)
+    inputStr = inputStr:lower()
+    return inputStr == "off" or
+        inputStr == "false" or
+        inputStr == "disable" or
+        inputStr == "no" or
+        inputStr == "0" or
+        inputStr == "" or
+        inputStr == "null" or
+        inputStr == "nil"
+end
+
+return UserInput
