f3d95e18619ec731ca4f35c7c12409d3138a3aaa
SirCabby <77202713+SirCabby@users.noreply.github.com>
2024-01-20 23:24:34 -0500

    better save management

--- cabby/configs/meleeStateConfig.lua
+++ cabby/configs/meleeStateConfig.lua
@@ -19,27 +19,37 @@ local function getConfigSection()
 end
 
 local function initAndValidate()
+    local taint = false
     if getConfigSection() == nil then
         DebugLog("MeleeStateConfig Section was not set, updating...")
         MeleeStateConfig._.config:GetConfigRoot()[MeleeStateConfig.key] = {}
+        taint = true
     end
 
     local configRoot = getConfigSection()
 
     if configRoot.stick == nil then
         configRoot.stick = true
+        taint = true
     end
 
     if configRoot.stick_distance == nil then
         configRoot.stick_distance = 12
+        taint = true
     end
 
     if configRoot.engage_range == nil then
         configRoot.engage_range = 50
+        taint = true
     end
 
     if configRoot.actions == nil then
         configRoot.actions = {}
+        taint = true
+    end
+
+    if taint then
+        MeleeStateConfig._.config:SaveConfig()
     end
 end
 
