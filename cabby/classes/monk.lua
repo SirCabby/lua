157687dbfebaabd9dcf0db301fd47a3c37f4392d
SirCabby <77202713+SirCabby@users.noreply.github.com>
2024-01-28 16:36:37 -0500

    toggle off broken code

--- cabby/classes/monk.lua
+++ cabby/classes/monk.lua
@@ -17,29 +17,29 @@ local function GetCombatAbilityFunction(abilityName)
 end
 
 local function RegisterCombatAbilities()
-    if mq.TLO.Me.Level() >= 30 and Character.HasAbility("Flying Kick") then
-        MeleeState.RegisterAbility(GetCombatAbilityFunction("Flying Kick"))
-    elseif mq.TLO.Me.Level() >= 25 and Character.HasAbility("Tail Rake") then
-        MeleeState.RegisterAbility(GetCombatAbilityFunction("Tail Rake"))
-    elseif mq.TLO.Me.Level() >= 25 and Character.HasAbility("Dragon Punch") then
-        MeleeState.RegisterAbility(GetCombatAbilityFunction("Dragon Punch"))
-    elseif mq.TLO.Me.Level() >= 20 and Character.HasAbility("Eagle Strike") then
-        MeleeState.RegisterAbility(GetCombatAbilityFunction("Eagle Strike"))
-    elseif mq.TLO.Me.Level() >= 10 and Character.HasAbility("Tiger Claw") then
-        MeleeState.RegisterAbility(GetCombatAbilityFunction("Tiger Claw"))
-    elseif mq.TLO.Me.Level() >= 5 and Character.HasAbility("Round Kick") then
-        MeleeState.RegisterAbility(GetCombatAbilityFunction("Round Kick"))
-    else
-        MeleeState.RegisterAbility(GetCombatAbilityFunction("Kick"))
-    end
+    -- if mq.TLO.Me.Level() >= 30 and Character.HasAbility("Flying Kick") then
+    --     MeleeState.RegisterAbility(GetCombatAbilityFunction("Flying Kick"))
+    -- elseif mq.TLO.Me.Level() >= 25 and Character.HasAbility("Tail Rake") then
+    --     MeleeState.RegisterAbility(GetCombatAbilityFunction("Tail Rake"))
+    -- elseif mq.TLO.Me.Level() >= 25 and Character.HasAbility("Dragon Punch") then
+    --     MeleeState.RegisterAbility(GetCombatAbilityFunction("Dragon Punch"))
+    -- elseif mq.TLO.Me.Level() >= 20 and Character.HasAbility("Eagle Strike") then
+    --     MeleeState.RegisterAbility(GetCombatAbilityFunction("Eagle Strike"))
+    -- elseif mq.TLO.Me.Level() >= 10 and Character.HasAbility("Tiger Claw") then
+    --     MeleeState.RegisterAbility(GetCombatAbilityFunction("Tiger Claw"))
+    -- elseif mq.TLO.Me.Level() >= 5 and Character.HasAbility("Round Kick") then
+    --     MeleeState.RegisterAbility(GetCombatAbilityFunction("Round Kick"))
+    -- else
+    --     MeleeState.RegisterAbility(GetCombatAbilityFunction("Kick"))
+    -- end
 
-    if mq.TLO.Me.Level() >= 6 and Character.HasAbility("Intimidation") then
-        MeleeState.RegisterAbility(GetCombatAbilityFunction("Intimidation"))
-    end
+    -- if mq.TLO.Me.Level() >= 6 and Character.HasAbility("Intimidation") then
+    --     MeleeState.RegisterAbility(GetCombatAbilityFunction("Intimidation"))
+    -- end
 
-    if mq.TLO.Me.Level() >= 10 and Character.HasAbility("Disarm") then
-        MeleeState.RegisterAbility(GetCombatAbilityFunction("Disarm"))
-    end
+    -- if mq.TLO.Me.Level() >= 10 and Character.HasAbility("Disarm") then
+    --     MeleeState.RegisterAbility(GetCombatAbilityFunction("Disarm"))
+    -- end
 end
     -- FD 17
     -- Mend 1
