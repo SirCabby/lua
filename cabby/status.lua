2474e55308be3b0db855fafb121762f130dddb20
SirCabby <77202713+SirCabby@users.noreply.github.com>
2024-02-19 14:54:44 -0500

    fix race condition

--- cabby/status.lua
+++ cabby/status.lua
@@ -5,7 +5,7 @@ local Status = {}
 Status.IsFacingTarget = function()
     if mq.TLO.Target == nil then return false end
 
-    local calc = math.abs(mq.TLO.Target.HeadingTo.DegreesCCW() - mq.TLO.Me.Heading.DegreesCCW())
+    local calc = math.abs((mq.TLO.Target.HeadingTo.DegreesCCW() or 0) - mq.TLO.Me.Heading.DegreesCCW())
     return calc < 50 or calc > 310 -- requires heading difference < 56, so plan for < 50, or > 310 for the wrap-around
 end
 
