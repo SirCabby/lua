65787aaf96b4b1bab2753bda43c3f3817f2a34fc
SirCabby <77202713+SirCabby@users.noreply.github.com>
2024-02-19 18:17:13 -0500

    state planning tweaks

--- cabby/setup.lua
+++ cabby/setup.lua
@@ -115,18 +115,27 @@ local function ClassSetup(stateMachine)
     Global.tracing.close(ftkey)
     
 -- | 1 My commands / Task / DZ
+
 -- | 19 Passive Mode
+
 -- | 29 Cure
 -- | 39 Heal
+
 -- | 49 Pulling
+
 -- | -- IN COMBAT --
 -- | 59 Mez
+
 -- | 69 Tank / grab aggro
+
 -- | 79 Dps
+
 -- | -- OUT COMBAT
 -- | 89 Looting
+
 -- | 99 Anchor
 -- | 109 Following
+
 -- | 119 Buff
 -- | 129 Misc
 end
